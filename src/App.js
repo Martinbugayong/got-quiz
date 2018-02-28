626b893e2b96c992fb8ce2b7240ce6b059acad36
Martin Bugayong <martinbugayong@gmail.com>
2018-02-28 10:33:03 -0800

    Add login/signup functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import userService from './utils/userService';
+import {Switch, Route} from 'react-router-dom';
+import SignUpForm from './components/SignUpForm/SignUpForm';
+import LoginForm from './components/LoginForm/LoginForm';
 
-class App extends Component {
+
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: null});
+  }
+
+  handleSignup = () => {
+    this.setState({
+      user: userService.getUser()
+    });
+  }
+
+  handleLogin = () => {
+    this.setState({
+      user: userService.getUser()
+    });
+  }
+
+  /*---------- Lifecycle Methods ----------*/
+
+  componentDidMount() {
+    let user = userService.getUser();
+    this.setState({user});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar handleLogout={this.handleLogout} user={this.state.user} />
+        <h1>GoT Quiz</h1>
+        <Switch>
+          <Route exact path='/signup' render={(props) => 
+            <SignUpForm 
+              {...props}
+              handleSignup={this.handleSignup}
+            />
+          } />
+          <Route exact path='/login' render={(props) => 
+            <LoginForm 
+              {...props}
+              handleLogin={this.handleLogin}
+            />
+          } />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const NavBar = (props) => {
+    let nav = props.user ?
+    <div>
+        <span>Hello, {props.user.name}</span>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/' onClick={props.handleLogout} >LOG OUT</Link>
+    </div>
+    :
+    <div>
+        <Link to ='/login'> LOG IN</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/signup' className='NavBar-link'>SIGN UP</Link>
+    </div>
+
+    return (
+        <div>
+            {nav}
+        </div>
+    );
+};
+
+export default NavBar;
